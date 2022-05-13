a4984a07cc7338bb2ab19a1eed51bcb6b6e98898
lillly02v <lilly02v@naver.com>
2022-05-13 15:12:30 +0900

    feat: Clock Modified with forEach

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,13 @@
-const clock = document.querySelector("h5#clock");
+const clock = document.querySelectorAll("h5#clock");
 
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}`;
+  clock.forEach(x =>{
+    x.innerText = `${hours}:${minutes}`
+  });
+    
 }
 
 getClock();
